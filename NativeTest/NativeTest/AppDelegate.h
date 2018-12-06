d0c37e91b1453d81c693020299ea34769918de78
nagender04031989 <nps012@gmail.com>
2018-12-06 16:27:36 +0530

    Native Test with MiniApp

---
+++ NativeTest/NativeTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NativeTest
+//
+//  Created by Priya Kapoor on 06/12/18.
+//  Copyright © 2018 QSS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ NativeTest/NativeTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  NativeTest
+//
+//  Created by Priya Kapoor on 06/12/18.
+//  Copyright © 2018 QSS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
